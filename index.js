835d2224c1eb3136fd61a4529da2896009231392
Shaik Kamil <shaikm.kamil@gmail.com>
2022-01-10 22:23:02 -0500

    commit tree cdcabac4c3a5aa3e2501dec0f263c4b03b5d784d

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const headquarters = 42;
+const feet = 264;
+
+function distanceFromHqInBlocks(num1) {
+if (num1 > 42) {
+    return num1-headquarters;
+}else if (num1 < 42) {
+    return headquarters-num1;
+}
+}
+distanceFromHqInBlocks(43);
+distanceFromHqInBlocks(34);
+
+function distanceFromHqInFeet(num2) {
+  if (num2 > feet){
+    return num2-feet +1;
+  }else if (num2 < feet) {
+        return feet-num2;
+}
+}
+distanceTravelledInFeet(43);
+
+function distanceTravelledInFeet(start, destination) {
+    //returns the number of feet traveled
+  }
+
+  function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+  }
